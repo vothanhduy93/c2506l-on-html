342bdbdda9bd7f44931ab2752000f68b7d4df8c1
vothanhduy93 <hcmc.duyvo@gmail.com>
2025-11-29 23:04:14 +0700

    29-11-2025

---
+++ buoi11/js/main.js
@@ -0,0 +1,51 @@
+// alert("Đây là file main.js");
+
+// console.log(3 > 2);
+
+// Declare variable: khai báo biến (nghĩa là tạo biến mà không có gán giá trị cho biến)
+// let x;
+// console.log("Giá trị của x là: " + x); // dấu + trong biểu thức có chuỗi thì gọi phép nối chuỗi
+// console.log("Kiểu dữ liệu của x là: " + typeof x);
+
+// Initialize variable: khởi tạo biến (nghĩa là tạo biến và có gán giá trị cho biến)
+// let y = 5;
+// console.log("Giá trị của y là: " + y);
+// console.log("Kiểu dữ liệu của y là: " + typeof y);
+
+// let z = "A";
+// console.log("Giá trị của z là: " + z);
+// console.log("Kiểu dữ liệu của z là: " + typeof z);
+
+// let a = "hello world";
+// console.log(a);
+// console.log("Kiểu dữ liệu của a là: " + typeof a);
+
+// let b = 9.5;
+// console.log(b);
+// console.log("Kiểu dữ liệu của b là: " + typeof b);
+
+// let c = true;
+// console.log("Kiểu dữ liệu của c là: " + typeof c);
+
+// let d = null;
+// console.log("Kiểu dữ liệu của d là: " + typeof d);
+
+// console.log("5" == 5);
+
+//let x = 5;
+// x++; // x = 6;
+// x = x + 1;
+
+// console.log("Giá trị ban đầu của x là: " + x);
+// console.log("Giá trị của x++ là: " + x++);
+// console.log("Giá trị cuối cùng của x là: " + x);
+// console.log("Giá trị cuối cùng của x là: " + x);
+
+// console.log(x++ + 5);
+// console.log(x++ + 5 + ++x);
+
+let x;
+console.log(x);
+console.log(typeof x);
+let y = (x ??= 10);
+console.log(y);
